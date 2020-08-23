ea1a08dc528454bb4550945e6bb576fb5c1d2a53
Sebastian Steinfeld <Sebastian.Steinfeld@rewe-group.com>
2020-08-23 22:15:42 +0200

    adding webpack.config.js, index.html, main.ts add scripts to package.json

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+const renderGreeting = () => {
+    const element = document.getElementById('greeting-container');
+    if(element) {
+        element.innerHTML = `
+            <h1>helloworld</h1>
+        `;
+    }
+}
+
+renderGreeting();
